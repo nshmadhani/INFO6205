44ec6e30507d973b51de2db1ef649c72c2b6c25d
Nishay Madhani <nshmadhani@gmail.com>
2021-09-11 18:05:51 -0400

    Assignment 0 is DOne

--- src/main/java/edu/neu/coe/info6205/functions/Newton.java
+++ src/main/java/edu/neu/coe/info6205/functions/Newton.java
@@ -52,12 +52,12 @@ public class Newton {
     public static void main(String[] args) {
 
         // Build the Newton's Approximation problem to be solved: cos(x) = x
-        Newton newton = new Newton("cos(x) - x", (double x) -> Math.cos(x) - x, (double x) -> -Math.sin(x) - 1);
+        Newton newton = new Newton("x^2*e^x", (double x) -> Math.pow(x,2) * Math.exp(x), (double x) -> Math.exp(x) * (Math.pow(x,2) + 2*x));
 
         // Solve the problem starting with a value of x = 1;
         // requiring a precision of 10^-7;
         // and giving up after 200 tries.
-        Either<String, Double> result = newton.solve(1.0, 200, 1E-7);
+        Either<String, Double> result = newton.solve(10.0, 200, 1E-7);
 
         // Process the result
         result.apply(
